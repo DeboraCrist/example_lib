6fcd23c1d130acb016508920c99e38243fcf912a
DeboraCrist <deboraccrist70@gmail.com>
2023-08-30 12:37:20 -0300

    requesito 1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,14 +6,19 @@ import 'package:example_lib/sgp.dart';
 void main() {
   var sgp = SGP();
 
-  var product1 = Product('p1', 'Product 1', 10.0);
-  var product2 = Product('p2', 'Product 2', 20.0);
+  var pizza = Pizza('p1', 'Margherita Pizza', 15.0);
+  var limonade = Beverage('b1', 'Limonade', 3.0);
+  var water = Beverage('b2', 'Water', 1.5);
+  var wine = Beverage('b3', 'Wine', 20.0);
+  var tShirt = FranchiseItem('f1', 'T-Shirt', 25.0);
+  var mug = FranchiseItem('f2', 'Mug', 10.0);
 
-  sgp.products = [product1, product2];
+  sgp.products = [pizza, limonade, water, wine, tShirt, mug];
 
   var order = Order(1, [
     OrderItem('p1', 2),
-    OrderItem('p2', 2),
+    OrderItem('b1', 3),
+    OrderItem('f2', 1),
   ]);
 
   sgp.addOrder(order);
--- lib/product.dart
+++ lib/product.dart
@@ -2,6 +2,28 @@ class Product {
   final String id;
   final String name;
   final double price;
+  final ProductCategory category; // Novo campo para categoria
 
-  Product(this.id, this.name, this.price);
+  Product(this.id, this.name, this.price, this.category);
+}
+
+enum ProductCategory {
+  Pizza,
+  Beverage,
+  FranchiseItem,
+}
+
+class Pizza extends Product {
+  Pizza(String id, String name, double price)
+      : super(id, name, price, ProductCategory.Pizza);
+}
+
+class Beverage extends Product {
+  Beverage(String id, String name, double price)
+      : super(id, name, price, ProductCategory.Beverage);
+}
+
+class FranchiseItem extends Product {
+  FranchiseItem(String id, String name, double price)
+      : super(id, name, price, ProductCategory.FranchiseItem);
 }
