5dec6ab2603ef64e1242d5e6bcdafddec6db6b4d
DeboraCrist <deboraccrist70@gmail.com>
2023-08-30 11:29:44 -0300

    testando criar um biblioteca

---
+++ lib/src/example_lib.dart
@@ -0,0 +1,5 @@
+library example_lib;
+
+export 'package:example_lib/src/calculator/multiplication.dart';
+export 'package:example_lib/src/content/example_widget.dart';
+
---
+++ test/example_lib_test.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:example_lib/src/example_lib.dart';
